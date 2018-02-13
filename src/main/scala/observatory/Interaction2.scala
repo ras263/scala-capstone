8a4424120f4e751732ec062215e3e7c2ee4b4081
Lakhno Anton <ras263.13@gmail.com>
2018-02-13 08:06:26 +0300

    Implementing all method of the milestone #6 at a single commit.

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -9,7 +9,34 @@ object Interaction2 {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Seq(
+      Layer(
+        LayerName.Temperatures,
+        Seq(
+          (60.0, Color(255, 255, 255)),
+          (32.0, Color(255, 0, 0)),
+          (12.0, Color(255, 255, 0)),
+          (0.0, Color(0, 255, 255)),
+          (-15.0, Color(0, 0, 255)),
+          (-27.0, Color(255, 0, 255)),
+          (-50.0, Color(33, 0, 107)),
+          (-60.0, Color(0, 0, 0))
+        ),
+        1975 to 2015
+      ),
+      Layer(
+        LayerName.Deviations,
+        Seq(
+          (7.0, Color(0, 0, 0)),
+          (4.0, Color(255, 0, 0)),
+          (2.0, Color(255, 255, 0)),
+          (0.0, Color(255, 255, 255)),
+          (-2.0, Color(0, 255, 255)),
+          (-7.0, Color(0, 0, 255))
+        ),
+        1975 to 2015
+      )
+    )
   }
 
   /**
@@ -17,7 +44,7 @@ object Interaction2 {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
@@ -29,7 +56,10 @@ object Interaction2 {
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+    val year = sliderValue()
+    if (year <= selectedLayer().bounds.min) Signal(selectedLayer().bounds.min)
+    else if (year >= selectedLayer().bounds.max) Signal(selectedLayer().bounds.max)
+    else Signal(year)
   }
 
   /**
@@ -38,7 +68,7 @@ object Interaction2 {
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"generated/${selectedLayer().layerName.id}/${selectedYear()}/{z}/{x}/{y}.png")
   }
 
   /**
@@ -47,7 +77,7 @@ object Interaction2 {
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"${selectedLayer().layerName.id.capitalize} (${selectedYear()})")
   }
 
 }
